9cd4f1d366538a8dbfc6b950b0b3979cfce9db6b
nic <nwerneck@gmail.com>
2019-03-14 23:57:51 +0100

    Some improvements on the merge code for medium arrays

--- src/medium.jl
+++ src/medium.jl
@@ -18,118 +18,46 @@ using ChipSort
 # data = rand(Int8, 16*64)
 # @code_warntype merge_vecs_tree(data, Val(16), Val(8), Val(8))
 
-# data = rand(Int8, 64*16);
-# @btime chipsort_medium($data, Val(16), Val(8), Val(8));
-# @btime sort($data);
-# data = rand(Int8, 64*8);
-# @btime chipsort_medium($data, Val(8), Val(8), Val(8));
-# @btime sort($data);
-# data = rand(Int8, 64*4);
-# @btime chipsort_medium($data, Val(4), Val(8), Val(8));
-# @btime sort($data);
-# data = rand(Int8, 64*2);
-# @btime chipsort_medium($data, Val(2), Val(8), Val(8));
-# @btime sort($data);
-
-
-
-
-
 # T=Int32
-# C=8
-# N=8
-# L=8
+# V=8
+# J=8
+# K=64
 
-# data = valloc(T, div(32, sizeof(T)), C*N*L)
-# data .= rand(T, C*N*L)
+# data = valloc(T, div(32, sizeof(T)), V*J*K)
+# data .= rand(T, V*J*K)
+# # data .= [1:4:32;2:4:32;3:4:32;4:4:32;]
 
-# qq = chipsort_medium(data, Val(C), Val(N), Val(L))
+# qq = chipsort_medium(data, Val(V), Val(J), Val(K))
 # reshape(qq, 16,:)
 
-# @code_native chipsort_medium(data, Val(C), Val(N), Val(L))
+# @assert chipsort_medium(data, Val(V), Val(J), Val(K)) == sort(data)
+
+# @code_warntype chipsort_medium(data, Val(V), Val(J), Val(K))
 
 
-
-function base(reps)
-    for _ in 1:reps
-        data = rand(Int32, 128*64)
-        sort(data)
+function base(data)
+    for d in length(data)
+        sort(data[d])
     end
 end
 
-function mine(reps)
-    for _ in 1:reps
-        data = rand(Int32, 128*64)
-        chipsort_medium(data, Val(128), Val(8), Val(8))
+function mine(data)
+    for d in length(data)
+        chipsort_medium(data[d], Val(4), Val(4), Val(128))
     end
 end
 
-display(@benchmark base(10))
-display(@benchmark mine(10))
+data = [rand(Int64, 16*128) for _ in 1:1000]
+
+base(data) == mine(data)
+
+display(@benchmark base($data))
+display(@benchmark mine($data))
 
 
-
-
-using Profile
-using ProfileView
-Profile.clear()
-@profile mine(1000)
-# Profile.print()
-ProfileView.view()
-
-
-
-
-# data = rand(T, 64*64)
-# @code_native
-# chipsort_medium(data, Val(C), Val(N), Val(L)) == sort(data)
-
-
-# println("First pass")
-# aa = [x for _ in 1:C*L for x in sort(rand(T,N))]
-# oo = zeros(T, C*L*N)
-
-# C = C >> 1
-# bitonic_merge_interleaved(
-#     ntuple(c->pointer(oo, 1+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(oo, 1+N+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(aa, 1+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(aa, 1+L*N+(2*(c-1))*L*N), C),
-#     Val(N)
-# )
-
-# display(reshape(aa,N*L,:))
-# display(reshape(oo,N*L*2,:))
-
-# println("second pass")
-# L = L << 1
-# C = C >> 1
-# aa = oo
-# oo = zeros(T, 2*C*L*N)
-
-# bitonic_merge_interleaved(
-#     ntuple(c->pointer(oo, 1+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(oo, 1+N+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(aa, 1+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(aa, 1+L*N+(2*(c-1))*L*N), C),
-#     Val(N)
-# )
-
-# display(reshape(oo,N*L*2,:))
-
-# bitonic_merge_interleaved(
-#     ntuple(c->pointer(oo, 1+N+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(oo, 1+2*N+(2*(c-1))*L*N), C),
-#     ntuple(c->pointer(oo, 1+N+(2*(c-1))*L*N), C),
-#     ntuple(c->
-#            if aa[1+N+(2*(c-1))*L*N] < aa[1+N+L*N+(2*(c-1))*L*N]
-#            pointer(aa, 1+N+(2*(c-1))*L*N)
-#            else
-#            pointer(aa, 1+N+L*N+(2*(c-1))*L*N)
-#            end
-#            , C),
-#     Val(N)
-# )
-
-
-# display(reshape(oo,N*L*2,:))
+# using Profile
+# using ProfileView
+# Profile.clear()
+# @profile mine(data)
+# # Profile.print()
+# ProfileView.view()
